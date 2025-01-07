1fca4f4b8656544514220f73bf2fdcc50747347c
erika <erika.ehorvathova@gmail.com>
2025-01-07 20:46:02 +0100

    zle :D

--- zaklad.py
+++ zaklad.py
@@ -6,7 +6,7 @@ Po cca 20 minutách se prostřídáte.
 Základ:
 Hra, kde máte peníze - 100 Kč.
 Je možné chodit mezi lokacemi (Hradčany, Václavák, Holešovice)
-Při přechodu do jiné lokace se přičte počet dnů.
+Při přechodu do jiné lokace je přičte počet dnů.
 Po 14-ti dnech hra končí
 Vypíše se finální počet peněz."""
 
@@ -14,8 +14,13 @@ print("Vítejte ve hře")
 
 pocatecni_zustatek = 100
 
-class lokace():
-    def
+class Lokace():
+    def __init__ (self,lokace):
 
+        Hradčany = Lokace
+        Václavák = Lokace
+        Holešovice = Lokace
 
+input("zadaj lokace kam chceš isť")
+print(f"zadal si lokace {Lokace}")
 
