043fb7f53002d06764ec09def0c1e7cf9c15618a
MirassariM09 <miroslavrezabek1@gmail.com>
2025-01-07 20:28:18 +0100

    prvni commit

---
+++ zaklad.py
@@ -0,0 +1,13 @@
+"""Zadání pro mini-projekt.
+Výstupem teamu je jeden kód.
+Vždy jeden sdílí obrazovku a ostatní mu radí.
+Po cca 20 minutách se prostřídáte.
+
+Základ:
+Hra, kde máte peníze - 100 Kč.
+Je možné chodit mezi lokacemi (Hradčany, Václavák, Holešovice)
+Při přechodu do jiné lokace je přičte počet dnů.
+Po 14-ti dnech hra končí
+Vypíše se finální počet peněz."""
+
+print("vítejte ve hře")
